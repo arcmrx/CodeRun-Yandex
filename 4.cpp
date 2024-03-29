ae9ec09ee5fe624ec355f9a66254842dcea06535
arcmrx <arturvolkov44@gmail.com>
2024-03-30 01:01:05 +0300

    4

--- 4.cpp
+++ 4.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <vector>
 
-int countPaths(int N, int M) {
+int countPaths(int N, int M)
+{
     std::vector<std::vector<int>> dp(N, std::vector<int>(M, 0));
     dp[0][0] = 1;
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < M; ++j) {
+    for (int i = 0; i < N; ++i)
+    {
+        for (int j = 0; j < M; ++j)
+        {
             if (i + 1 < N && j + 2 < M)
                 dp[i + 1][j + 2] += dp[i][j];
             if (i + 2 < N && j + 1 < M)
@@ -14,7 +17,8 @@ int countPaths(int N, int M) {
     }
     return dp[N - 1][M - 1];
 }
-int main() {
+int main()
+{
     int N, M;
     std::cin >> N >> M;
     std::cout << countPaths(N, M);
